537154265d7ba953a0097afd30c9054fac2a22cd
Yll2104 <bytylc24@htl-kaindorf.at>
2025-03-08 23:53:46 +0100

    Copying array into temporary array

--- Array.c
+++ Array.c
@@ -1,5 +1,44 @@
 #include <stdio.h>
 
 int main(){
+    int Size;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &Size);
+
+    int arr[Size];
+    int R_Arr[(Size + 1) / 2];
+    int L_Arr[Size / 2];
+
+    int m = Size / 2;
+
+    for(int i = 0; i < Size; i++){
+        printf("Enter the element %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        L_Arr[i] = arr[i];
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        R_Arr[i] = arr[i + m];
+    }
+    
+
+    printf("The Left Array is: ");
+    for (int i = 0; i < m; i++)
+    {
+        printf("%d ", L_Arr[i]);
+    }
+    printf("\n");
+    printf("The Right Array is: ");
+    for (int i = 0; i < m; i++)
+    {
+        printf("%d ", R_Arr[i]);
+    }
+    
     return 0;
 }
