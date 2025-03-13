3348e98db8d12b681b458cc5aa5f1db83119cb36
Yll2104 <bytylc24@htl-kaindorf.at>
2025-03-13 19:54:00 +0100

    For Loop for searching the lowest element in an array

---
+++ SelectionSort.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int array[5] = {6, 5, 8, 6, 3};
+
+    int min = 0;
+
+    for (int i = 0; i < 4; i++)
+    {
+        if (array[i] > array[i + 1])
+        {
+            min = array[i + 1];
+        }
+    }
+
+    printf("The lowest Element is: %d", min);
+    return 0;
+}
